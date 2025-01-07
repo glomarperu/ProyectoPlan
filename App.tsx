f9364bf24c9743b34ce95c666670be51999c69a2
glomarperu <glomarperu1@gmail.com>
2025-01-07 08:45:41 -0500

    Creando el Home

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { HomeScreen } from './src/screens/home/HomeScreen';
 
 function App(): React.JSX.Element {  
 
  return (   
       <View>
         <Text>
-          Hola Mundo, primer campo
+          <HomeScreen/>
         </Text>
       </View>
   );

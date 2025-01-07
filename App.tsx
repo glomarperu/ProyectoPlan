ced2e816f1a0a45bb79f1ffc3e77f9dbe1229b98
glomarperu <glomarperu1@gmail.com>
2025-01-07 01:39:01 -0500

    Creando mi Hola Mundo

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+function App(): React.JSX.Element {  
+
+ return (   
+      <View>
+        <Text>
+          Hola Mundo, primer campo
+        </Text>
+      </View>
+  );
+}
+
+export default App;

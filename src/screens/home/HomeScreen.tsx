b1641d9063b927e05dd2d64d355d1e64735f38ed
LeonardoGrande <leofgh18@gmail.com>
2025-01-07 19:04:11 -0500

    Mejorando el Home

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, Pressable, Image } from 'react-native';
 import auth from '@react-native-firebase/auth';
 import { CommonActions, useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
@@ -10,6 +10,7 @@ import { styles } from '../../theme/styles';
 export const HomeScreen = () => {  // se exporta la funcion
 
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>(); // se usa el hook para obtener la navegacion
+  const [userName, setUserName] = useState<string | null>(null); // se declara el estado para el nombre de usuario
 
   const logout = async () => {
     try {
@@ -27,19 +28,36 @@ export const HomeScreen = () => {  // se exporta la funcion
     }
   };
 
+  useEffect(() => { // se usa el hook para obtener el nombre de usuario
+    const currentUser = auth().currentUser; // se obtiene el usuario actual
+    if (currentUser) { // si el usuario existe
+      setUserName(currentUser.displayName || currentUser.email); // se establece el nombre de usuario
+    }
+  }, []); // se ejecuta solo una vez al cargar la pantalla
+
   // eliminos haciones de consola y agrego las funciones del ButtonComponent para ejecutar las vistas
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenido</Text>
+      <Text style={styles.title}>Bienvenido: {userName ? userName : 'Usuario'}</Text>
       <ButtonComponent onAction={logout} label="Cerrar Sesión" />
-      <ButtonComponent
-        label="Agregar Tarea"
-        onAction={() => navigation.navigate('AddTask')}
-      />
-      <ButtonComponent
-        label="Lista de Tareas"
-        onAction={() => navigation.navigate('TaskList')}
-      />
+      <View style={styles.cardContainer}>
+        <Pressable
+          style={[styles.card, { backgroundColor: '#4CAF50' }]}
+          onPress={() => navigation.navigate('AddTask')}>
+          <Image
+            source={require('../../img/add.png')} // Ajusta la ruta a tu imagen
+            style={styles.icon} />
+          <Text style={styles.cardText}>Agregar Tarea</Text>
+        </Pressable>
+        <Pressable
+          style={[styles.card, { backgroundColor: '#FF9800' }]}
+          onPress={() => navigation.navigate('TaskList')}>
+          <Image
+            source={require('../../img/list.png')} // Ajusta la ruta a tu imagen
+            style={styles.icon} />
+          <Text style={styles.cardText}>Lista de Tareas</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };

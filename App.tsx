f91ad08d4f81a7a999b61b84786e42dbd1c29468
glomarperu <glomarperu1@gmail.com>
2025-01-13 20:13:59 -0500

    Solucionando el error del cerrar sesión

--- App.tsx
+++ App.tsx
@@ -3,17 +3,30 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigation } from './src/routes/StackNavigation';
 import auth from '@react-native-firebase/auth';
 import { TaskProvider } from './src/context/TaskContext';
+import { Text, View } from 'react-native';
 
 function App(): React.JSX.Element {
+
+  const [initializing, setInitializing] = useState(true);
   const [user, setUser] = useState<any>(null);
+
   const onAuthStateChanged = (user: any) => {
     console.log('Estado de autenticación cambiado:', user);
     setUser(user);
+    if (initializing) setInitializing(false);
   };
   useEffect(() => {
     const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
     return subscriber;
   }, []);
+
+  if (initializing) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
 
   return (
     <TaskProvider>
